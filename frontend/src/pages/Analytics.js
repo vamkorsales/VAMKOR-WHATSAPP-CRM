9306a139e9fdc665715e107f094b8033a98ee417
Bonita Anderson <banderson651@insite.4cd.edu>
2026-04-30 18:01:40 +0530

    best ui and A0uth updated

---
+++ frontend/src/pages/Analytics.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Box, Typography, Paper } from '@mui/material';
+
+function Analytics() {
+  return (
+    <Box>
+      <Typography variant="h4" gutterBottom>Advanced Analytics</Typography>
+      <Paper sx={{ p: 3 }}>
+        <Typography>Heatmaps and ROI tracking coming soon.</Typography>
+      </Paper>
+    </Box>
+  );
+}
+
+export default Analytics;
---
+++ frontend/src/pages/Campaigns.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Box, Typography, Paper } from '@mui/material';
+
+function Campaigns() {
+  return (
+    <Box>
+      <Typography variant="h4" gutterBottom>Campaign Management</Typography>
+      <Paper sx={{ p: 3 }}>
+        <Typography>Campaign creation UI coming soon.</Typography>
+      </Paper>
+    </Box>
+  );
+}
+
+export default Campaigns;
